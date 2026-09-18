b8bc54feaa1090c7f53fe15abb3e90a1094a1da4
VANSHIKA870 <2k23csaiml2313462@gmail.com>
2026-09-18 12:27:37 +0530

    Time: 1 ms (99.1%), Space: 42.4 MB (84.69%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -3,14 +3,15 @@ class Solution {
         if(n<=0){
             return false;
         }
-        while(n%2==0){
-            n=n/2;
+        
+        if(n%2==0){
+            return isUgly(n/2);
         }
-        while(n%3==0){
-            n=n/3;
+        if(n%3==0){
+            return isUgly(n/3);
         }
-        while(n%5==0){
-            n=n/5;
+        if(n%5==0){
+           return isUgly(n/5);
         }
         if(n==1){
             return true;
